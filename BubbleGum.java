24ba30ae10ed4a5a5696832a0894cf73acc08126
Anirudh Konidala <kidskodingclub@gmail.com>
2023-03-21 10:48:38 -0500

    world is fixed no more exception

---
+++ BubbleGum.java
@@ -0,0 +1,48 @@
+import greenfoot.*;
+public class BubbleGum extends Ability {
+    private static GreenfootImage img;
+    private boolean movingRight;
+    private int bounces;
+    
+    public BubbleGum() {
+        super(1000, 20);
+        img = this.getImage();
+        bounces = 0;
+        img.scale(img.getWidth() / 4, img.getHeight() / 4);
+        this.setImage(img);
+        movingRight = true;
+    }
+    
+    @Override
+    public void act() {
+        super.act();
+        if(this.isAtEdge()) {
+            if(this.getX() == 0) {
+                this.setLocation(this.getX() + 15, this.getY() + 10); 
+                movingRight = true;
+            }
+            else {
+                this.setLocation(this.getX() - 15, this.getY() + 10);
+                movingRight = false;
+            }
+            bounces++;
+            intersects = false;
+        }
+        else {
+            if(movingRight) {
+                this.setLocation(this.getX() + 15, this.getY());
+            }
+            else {
+                this.setLocation(this.getX() - 15, this.getY());
+            }
+        }
+        if(bounces == 4) {
+            this.pop();
+            return;
+        }
+        detectCollision("Balthazar");
+    }
+    public void pop() {
+        getWorld().removeObject(this);
+    }
+}
