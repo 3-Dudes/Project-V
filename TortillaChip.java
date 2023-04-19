aabceecf4e9177bfc431833796d267d1959fb5bb
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2023-04-19 11:58:35 -0500

    removed boolean flag, nullifying direction of the way tortilla chip would go

--- TortillaChip.java
+++ TortillaChip.java
@@ -2,6 +2,7 @@ import greenfoot.*;
 public class TortillaChip extends Weapon {
     private static int damage = 10;
     public static GreenfootImage img;
+    public static GreenfootImage left;
     private boolean right;
     public TortillaChip(boolean right) {
         super(new ElMacho());
@@ -10,15 +11,16 @@ public class TortillaChip extends Weapon {
         img.scale(img.getWidth() / 7, img.getHeight() / 7);
         img.mirrorHorizontally();
         setImage(img);
-    }    
+    }
+    public TortillaChip() {
+        super(new ElMacho());
+        img = this.getImage();
+        img.scale(img.getWidth() / 7, img.getHeight() / 7);
+        img.mirrorHorizontally();
+        setImage(img);
+    }
     public void act() {
-        if(right) {
-            this.setLocation(this.getX() + 8, this.getY());
-        }
-        else {
-            //getImage().mirrorHorizontally();
-            this.setLocation(this.getX() - 8, this.getY());    
-        }
+        this.setLocation(this.getX() + 8, this.getY());
         detectCollision("ElMacho", damage);
     }
 }
