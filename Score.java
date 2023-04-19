b0daf8576bddce77db979a1c13eebd1ed8d81ab6
Anirudh Konidala <kidskodingclub@gmail.com>
2023-04-18 19:53:21 -0500

    Added a white sheet cover over the score counter

--- Score.java
+++ Score.java
@@ -1,15 +1,20 @@
 import greenfoot.*;
 public class Score extends Actor {
     private GreenfootImage score;
+    private GreenfootImage cover;
     private int playerScore;
     private int cpuScore;
     public Score(int playerScore, int cpuScore) {
         score = new GreenfootImage(playerScore + " - " + cpuScore, 
             100, null, null);
-        setScore(score);
+        cover = new GreenfootImage(200, 75);
+        cover.setColor(Color.WHITE);
+        cover.fillRect(0, 0, cover.getWidth(), cover.getHeight());
+        cover.drawImage(this.score, 23, -12);
+        setScore(cover);
     }
-    public void setScore(GreenfootImage score) {
-        this.score = score;
-        this.setImage(this.score);
+    public void setScore(GreenfootImage cover) {
+        this.cover = cover;
+        this.setImage(cover);
     }
 }
