047c974d6b9b62beceb2f2be7ea318247195b4ae
Anirudh Konidala <kidskodingclub@gmail.com>
2023-04-23 12:20:01 -0500

    removed clarity of balthazar's pop method in bubble gum

--- BubbleGum.java
+++ BubbleGum.java
@@ -37,7 +37,7 @@ public class BubbleGum extends Ability {
             }
         }
         if(bounces == 4) {
-            this.pop();
+            pop();
             return;
         }
         detectCollision("Balthazar");
