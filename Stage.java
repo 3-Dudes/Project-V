d81f817e30ba6d91e66f61f49fefd88eb20509c6
Krishna <krishlearns@gmail.com>
2023-04-23 16:02:04 -0500

    added basic wrestling champ, needs fixes

--- Stage.java
+++ Stage.java
@@ -8,9 +8,9 @@ public class Stage extends World {
     }
     private void prepareLevel() {
         player = new ElMacho();
-        cpu = new Balthazar();
+        //cpu = new Balthazar();
         this.addObject(player, 200, this.getHeight() - 150);
-        this.addObject(cpu, 1000, this.getHeight() - 175);
+        //this.addObject(cpu, 1000, this.getHeight() - 175);
         this.setPaintOrder(Ability.class, Weapon.class, Player.class);
     }
 }
