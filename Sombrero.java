02166c7b587c47ad6febf41e86a9a1b6e7daf2d5
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2023-04-26 11:55:30 -0500

    Added Eduardo form second move: Sombrero

--- Sombrero.java
+++ Sombrero.java
@@ -1,12 +1,25 @@
 import greenfoot.*;
 public class Sombrero extends Ability {
     private int health;
+    private boolean intersects;
     public Sombrero() {
         super(800, 20);
         health = 100;
+        intersects = false;
         this.setRotation(15);
     }
     public int getHealth() {
         return health;
+    }
+    public void act() {
+        Ability a = (Ability) this.getOneIntersectingObject(null);
+        if(a != null && !intersects) {
+            health -= a.getDamage();
+            intersects = true;
+            getWorld().removeObject(a);
+        }     
+        if(health <= 0) {
+            getWorld().removeObject(this);
+        }
     }
 }
