d50af89d5e741cf07d204f6f088d1a96191583f7
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2023-05-02 15:34:24 -0500

    added a mode superclass for all the gamemodes in Project V

---
+++ Domination.java
@@ -0,0 +1,11 @@
+import greenfoot.*;
+public class Domination extends Mode {
+    //private Zone a;
+    //private Zone b;
+    //private Zone c;
+    public Domination()
+    {
+        //a = new Zone();
+        //b = new Zone();
+    }
+}
