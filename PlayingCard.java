578e0079c65a824a1e5ef130928fb8f3fa7bfb8e
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2023-05-05 12:02:56 -0500

    added lucy's next move: playing card. Alt fire move

---
+++ PlayingCard.java
@@ -0,0 +1,47 @@
+import greenfoot.*;
+public class PlayingCard extends Weapon {
+    private int tracker = 0;
+    private int ammo = 7;
+    public static GreenfootImage left;
+    public static GreenfootImage right;
+    private boolean isRight;
+    public PlayingCard(boolean isRight) {
+        super(2, 2);
+        tracker = 0;
+        this.isRight = isRight;
+    }
+    public void act() {
+        move();
+        if(this.isAtEdge()) {
+            getWorld().removeObject(this);
+        }
+        detectCollision("Lucy", 5);
+    }
+    public void move() {
+        switch(tracker) {
+            case 5:
+                this.setRotation(90);
+                break;
+            case 10:
+                this.setRotation(180);
+                break;
+            case 15:
+                this.setRotation(270);
+                break;
+            case 20:
+                this.setRotation(360);
+                break;
+            case 25:
+                tracker = 0;
+                this.setRotation(0);
+                break;
+        }
+        tracker++;
+        if(isRight) {
+            this.setLocation(this.getX() + 10, this.getY());    
+        }
+        else {
+            this.setLocation(this.getX() - 10, this.getY());
+        }
+    } 
+}
