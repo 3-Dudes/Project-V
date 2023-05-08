20cbd5da2589d89be9b3efcfce941f55f9dd2f0a
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2023-05-08 15:19:10 -0500

    added lipstick ability for lucy

---
+++ LipStick.java
@@ -0,0 +1,9 @@
+import greenfoot.*;
+public class LipStick extends Ability {
+    public LipStick() {
+        super(900, 10);
+    }
+    public void act() {
+        // Add your action code here.
+    }    
+}
