4f03b090883002a13831dfe9d2d58fe649e0359f
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2023-05-08 14:55:52 -0500

    added dimensions of the win screen

--- EndScreen.java
+++ EndScreen.java
@@ -3,6 +3,7 @@ public class EndScreen extends World {
     private Player winner;
     private Ability ab;
     public EndScreen(Player winner, Ability ab) {
+        super(1200, 800, 1);
         this.winner = winner;
         playAnimation();
     }
