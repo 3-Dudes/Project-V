1fa4a1d9a29622944d42133fa35111729da9804c
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2023-05-08 12:01:25 -0500

    adding end screen for game

---
+++ EndScreen.java
@@ -0,0 +1,15 @@
+import greenfoot.*;
+public class EndScreen extends World {
+    private Player winner;
+    private Ability ab;
+    public EndScreen(Player winner, Ability ab) {
+        this.winner = winner;
+        playAnimation();
+    }
+    private void makeScreen() {
+        
+    }
+    private void playAnimation() {
+        
+    }
+}
