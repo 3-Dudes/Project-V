7bf08a87bb98ef824ce0666ec607f67ff1640614
Krishna <krishlearns@gmail.com>
2023-05-09 07:39:35 -0500

    modified constructor-still need to add point counter

--- Stock.java
+++ Stock.java
@@ -12,10 +12,12 @@ public class Stock extends Mode
     private Player p1;
     private Player p2;
     public Stock(Player p1, Player p2) {
-        
+        this.p1=p1;
+        this.p2=p2;
     }
 
     public void act(){
+        //still need to add point counter
         if(lives1==0||lives2==0){
             if(lives1==0){
                 playWinAnimation(p);
