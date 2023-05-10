5be9b4d83faebb9d0329fe5cbbc028ffc6f0a306
Krishna <68204671+kidskoding@users.noreply.github.com>
2023-05-10 11:59:12 -0500

    fixing flying animation of vector's ultimate

--- FlyingV.java
+++ FlyingV.java
@@ -1,15 +1,27 @@
 import greenfoot.*;
 public class FlyingV extends UltimateAbility {
+    private static GreenfootImage left;
+    private static GreenfootImage right;
     public FlyingV() {
+        GreenfootImage img = getImage();
+        img.scale(img.getWidth() / 3, img.getHeight() / 3);
+        right = new GreenfootImage(img);
+        left = new GreenfootImage(right);
+        left.mirrorVertically();
+        this.setImage(left);
+    }
+    @Override
+    public void addedToWorld(World w) {
+        super.addedToWorld(w);
         this.setLocation(this.getX(), 100);
-        this.setRotation(120);
+        this.setRotation(-40);
     }
     public void act() {
-        this.setLocation(this.getX() + 5, this.getY() + 10);
+        this.setLocation(this.getX() + 5, this.getY() + 20);
         if(this.isAtEdge()) {
             if(this.getY() == getWorld().getHeight() - 1) {
-                this.setRotation(300);
-                this.setLocation(this.getX() + 5, this.getY() - 10);
+                this.setImage(right);
+                this.setLocation(this.getX() + 5, this.getY() - 20);
             }
             if(this.getY() == 0) {
                 setCharge(0);
