b4ac9cc4ab40bbf3c56d5808471eb3781347f2d9
Anirudh Konidala <kidskodingclub@gmail.com>
2023-05-09 21:59:33 -0500

    Readded vector and added animations

---
+++ Vector.java
@@ -0,0 +1,30 @@
+import greenfoot.*;
+public class Vector extends Player {
+    public Vector() {
+        super("Vector", 3, false, 300 ,300, "vector", 6);
+    }
+    @Override
+    public void c() {
+        
+    }
+    @Override
+    public void q() {
+        
+    }
+    @Override
+    public void e() {
+        
+    }
+    @Override
+    public void x() {
+        
+    }
+    @Override
+    public void singleFire() {
+        
+    }
+    @Override
+    public void burstFire() {
+        
+    }
+}
