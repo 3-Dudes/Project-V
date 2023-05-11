05f4c123cbdb9f1dc24556144bcd23c059896929
Krishna <77071056+Dac00ldude@users.noreply.github.com>
2023-05-11 11:56:01 -0500

    excess greenfoot image code removed

--- ModeSelect.java
+++ ModeSelect.java
@@ -16,14 +16,5 @@ public class ModeSelect extends World {
         this.addObject(dominationButton, 300, 550);
         this.addObject(stockButton, 600, 550);
         this.addObject(timeButton, 900, 550);
-        /* bg.drawImage(label(modeLabel, "Domination", -65, -40), 200, 550);
-        GreenfootImage stockedLabel = new GreenfootImage(200, 50);
-        stockedLabel.setColor(Color.ORANGE);
-        stockedLabel.fill();
-        bg.drawImage(label(stockedLabel, "Stock", -30, -40), 500, 550);
-        GreenfootImage timedLabel = new GreenfootImage(200, 50);
-        timedLabel.setColor(Color.ORANGE);
-        timedLabel.fill();
-        bg.drawImage(label(timedLabel, "Time", -30, -40), 800, 550); */
     }
 }
