455358cae3a72a11f194aecd20d80933df219726
Krishna <77071056+Dac00ldude@users.noreply.github.com>
2023-05-11 10:36:23 -0500

    added an x variable to control the transparency of the start button

--- FlyingV_Start.java
+++ FlyingV_Start.java
@@ -1,9 +1,11 @@
 import greenfoot.*;
 public class FlyingV_Start extends Actor {
+    private int x;
     public FlyingV_Start() {
         GreenfootImage img = getImage();
         img.scale(img.getWidth() / 2, img.getHeight() / 2);
         img.mirrorHorizontally();
+        x = 0;
     }
     public void act() {
         this.setLocation(this.getX() + 15, this.getY());
