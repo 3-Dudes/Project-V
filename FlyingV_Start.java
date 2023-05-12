58a683a03d7f5cda6155de98c77abf322e9136cc
Krishna <77071056+Dac00ldude@users.noreply.github.com>
2023-05-12 07:52:33 -0500

    added a fade transition into the start button on the startscreen

--- FlyingV_Start.java
+++ FlyingV_Start.java
@@ -1,16 +1,17 @@
 import greenfoot.*;
 public class FlyingV_Start extends Actor {
     private int x;
+    private StartButton button;
     public FlyingV_Start() {
         GreenfootImage img = getImage();
         img.scale(img.getWidth() / 3, img.getHeight() / 3);
         img.mirrorHorizontally();
         x = 0;
+        button = new StartButton();
     }
     public void act() {
         this.setLocation(this.getX() + 15, this.getY());
         if(this.isAtEdge()) {
-            StartButton button = new StartButton();
             getWorld().addObject(button, 610, 600);
             getWorld().removeObject(this);
         }
--- StartButton.java
+++ StartButton.java
@@ -1,12 +1,25 @@
 import greenfoot.*;
 public class StartButton extends Button
 {
+    private int x;
+    private GreenfootImage img;
     public StartButton() {
         super(Color.ORANGE, 200, 50, "START", -35, -40);
+        x = 0;
+        img = getImage();
+        img.setTransparency(x);
     }
     public void whenClicked() {
         if(Greenfoot.mouseClicked(this)) {
             Greenfoot.setWorld(new ModeSelect());
+        }
+    }
+    public void act() {
+        super.act();
+        if(x < 255) {
+            x += 5;
+            img.setTransparency(x);
+            this.setImage(img);
         }
     }
 }
