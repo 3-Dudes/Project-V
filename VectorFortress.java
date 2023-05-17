f9637f076a07949d8b181a1d71c2ee9ec3c3dee2
Anirudh Konidala <kidskodingclub@gmail.com>
2023-05-17 00:29:33 -0500

    added another stage - vector's fortress

---
+++ VectorFortress.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class VectorFortress here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class VectorFortress extends Stage
+{
+
+    /**
+     * Constructor for objects of class VectorFortress.
+     * 
+     */
+    public VectorFortress()
+    {
+    }
+}
